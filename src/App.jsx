8dad5d84d8f8b1126e11582c2df9022f6a1923a1
samflexx31 <samflex3124@gmail.com>
2023-12-15 12:58:49 +0100

    Header section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/Signin";
+import SignUp from "./pages/SignUp";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Header from "./components/Header";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
