e34619203766189d1ed95717f778a9d663121167
samflexx31 <samflex3124@gmail.com>
2023-12-26 09:37:39 +0100

    working on google auth

---
+++ client/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD218YJO8g9rMJsawwtNhfrESRi6eDC9oc",
+  authDomain: "mern-estate-8d6f1.firebaseapp.com",
+  projectId: "mern-estate-8d6f1",
+  storageBucket: "mern-estate-8d6f1.appspot.com",
+  messagingSenderId: "186793260507",
+  appId: "1:186793260507:web:e0a3b13003d667715d23fe",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
