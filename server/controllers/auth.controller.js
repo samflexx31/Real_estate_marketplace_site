5b0a82ae1b7a89b7a9d528ce65c0d8f654fd4b7b
samflexx31 <samflex3124@gmail.com>
2023-12-27 08:37:35 +0100

    update header and made profile private

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -44,12 +44,12 @@ export const google = async (req, res, next) => {
         .status(200)
         .json(rest);
     } else {
-      const generatePassword = math.random().toString(36).slice(-8);
+      const generatePassword = Math.random().toString(36).slice(-8);
       const hashedPassword = bcryptjs.hashSync(generatePassword, 10);
       const newUser = new User({
         username:
           req.body.name.split(" ").join("").toLowerCase() +
-          math.random().toString(36).slice(-4),
+          Math.random().toString(36).slice(-4),
         email: req.body.email,
         password: hashedPassword,
         avatar: req.body.photo,
