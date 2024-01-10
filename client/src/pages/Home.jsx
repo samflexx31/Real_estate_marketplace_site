36bfc2e2ca5aaab7e5b13f9847004efc2b7abaf4
samflexx31 <samflex3124@gmail.com>
2024-01-10 13:24:03 +0100

    add pre installation

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -87,7 +87,7 @@ const Home = () => {
       </Swiper>
       {/* listing results for offer, sales and rent */}
 
-      <div className="max-w-6xl mx-auto p3 flex flex-col gap-8 my-10">
+      <div className=" max-w-6xl mx-auto p-5 flex flex-col gap-8 my-10">
         {offerListing && offerListing.length > 0 && (
           <div className="">
             <div className="my-3">
